e22d7f23a913efd774d2ab0aa3cb250670cb841e
Luciana A. Fruin <luciana.fruin@gmail.com>
2022-06-09 19:40:42 +0100

    Add ranking by backlink.

--- src/services/search/search_result.js
+++ src/services/search/search_result.js
@@ -1,11 +1,13 @@
 "use strict";
 
+const becca = require('../../becca/becca');
 const beccaService = require('../../becca/becca_service');
 
 class SearchResult {
     constructor(notePathArray) {
         this.notePathArray = notePathArray;
         this.notePathTitle = beccaService.getNoteTitleForPath(notePathArray);
+        this.notePathLinks = becca.getNote(this.notePathArray[this.notePathArray.length - 1]).getTargetRelations();
     }
 
     get notePath() {
@@ -15,25 +17,25 @@ class SearchResult {
     get noteId() {
         return this.notePathArray[this.notePathArray.length - 1];
     }
+    
 
-    computeScore(tokens) {
+    computeScore(tokens) { // Token is query?
         this.score = 0;
 
-        // matches in attributes don't get extra points and thus are implicitly valued less than note path matches
-
         const chunks = this.notePathTitle.toLowerCase().split(" ");
+        const links = this.notePathLinks;
 
         for (const chunk of chunks) {
             for (const token of tokens) {
                 if (chunk === token) {
-                    this.score += 4 * token.length;
-                }
-                else if (chunk.startsWith(token)) {
-                    this.score += 2 * token.length;
+                    this.score += 100;
                 }
                 else if (chunk.includes(token)) {
-                    this.score += token.length;
+                    this.score += 30;
                 }
+                
+                this.score += links.length * this.score;
+                this.score += this.score / chunks.length;
             }
         }
     }
